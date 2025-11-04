1f953c5a34e450642ff0af461ea6bdc62e65bb2e
4everblue77 <109893202+4everblue77@users.noreply.github.com>
2025-11-04 13:19:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import streamlit as st
 
 
 
-SUPABASE_URL = st.secrets("SUPABASE_URL")
-SUPABASE_KEY = st.secrets("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 # Create Supabase client
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
