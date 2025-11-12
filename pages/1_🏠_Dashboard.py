77e8e2f76a49f57c437ed016f08790c7be97996f
4everblue77 <109893202+4everblue77@users.noreply.github.com>
2025-11-12 19:23:11 +0000

    Update 1_🏠_Dashboard.py

--- pages/1_🏠_Dashboard.py
+++ pages/1_🏠_Dashboard.py
@@ -85,5 +85,5 @@ else:
                 "day": selected_day,
                 "week": week_label
             }
-            st.switch_page("📄 Session Detail")
+            st.switch_page("Session_Detail")
         
