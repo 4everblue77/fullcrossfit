e2322629184b44f77bf72fee9785e99b53102979
4everblue77 <109893202+4everblue77@users.noreply.github.com>
2025-11-28 08:09:51 +0000

    Update benchmark.py

--- session_views/benchmark.py
+++ session_views/benchmark.py
@@ -60,7 +60,7 @@ def render(session):
 
         st.markdown(
             f"""
-            <div style="position:relative; width:100%; height:800; overflow:hidden;">
+            <div style=" width:100%; height:800; overflow:hidden;">
                 <iframe>
                     {wodwell_url}
                 </iframe>
