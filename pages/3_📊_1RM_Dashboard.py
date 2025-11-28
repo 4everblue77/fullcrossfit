69e87b374a068a217f2dea66ebc115f0d459192a
4everblue77 <109893202+4everblue77@users.noreply.github.com>
2025-11-28 11:46:53 +0000

    Update and rename 1RM_Dashboard.py to 3_📊_1RM_Dashboard.py

--- pages/3_📊_1RM_Dashboard.py
+++ pages/3_📊_1RM_Dashboard.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 from supabase import create_client
 
+
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
